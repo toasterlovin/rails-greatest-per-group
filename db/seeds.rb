74cdf16a7fc6f7fd18acaec3bba6e6a9da45edfc
Rico Jones <rico@toasterlovin.com>
2016-04-03 22:41:06 -0700

    Fix db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,7 +38,7 @@ austen.posts.create(
   created_at: Date.new(1817, 1, 1)
 )
 
-austen.posts.create(
+dickens.posts.create(
   title: "Oliver Twist",
   body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   created_at: Date.new(1837, 1, 1)
