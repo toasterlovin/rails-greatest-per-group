fdda2743f26e2eff53cef9a5d20166dd0284ef7e
Rico Jones <rico@toasterlovin.com>
2016-04-03 22:52:09 -0700

    Add custom query methods index view

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,13 @@ class AuthorsController < ApplicationController
   def index
     @authors = Author.all
   end
+
+  def custom_query_methods
+    @authors = Author
+      .select("authors.*, newest_posts.title AS newest_post_title")
+      .joins("LEFT JOIN (SELECT DISTINCT ON (author_id) *
+                         FROM posts
+                         ORDER BY author_id, created_at DESC)
+              AS newest_posts ON newest_posts.author_id = authors.id")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :authors, only: :index
+  resources :authors, only: :index do
+    collection do
+      get :custom_query_methods
+    end
+  end
 end
